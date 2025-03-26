9fc745e00f15f2e18c89964299a6a1679f4d9d04
Joe Kim <joesm.genie@gmail.com>
2025-03-26 23:24:49 +0700

    ascii code

--- basic.cpp
+++ basic.cpp
@@ -3,19 +3,13 @@
 using namespace std;
 
 int main() {
-    int age1;
-    int age2;
-    int sumAge;
+  char fistCharacter = 'H';
+  char secondCharacter = 'i';
+  char space = ' ';
+  char exclamationMark = '!';
 
-    cout << "Please enter the 1st person age: ";
-    cin >> age1;
+  cout << fistCharacter + secondCharacter + space + exclamationMark << endl;
+  // the output is ASCII code
 
-    cout << "Please enter the 2nd person age: ";
-    cin >> age2;
-
-    sumAge = age1 + age2;
-
-    cout << "The sum of age is " << sumAge << endl;
-
-    return 0;
+  return 0;
 }
