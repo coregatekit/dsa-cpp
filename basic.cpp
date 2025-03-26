10a8969cf261873937064e6aa266bba1ab7778be
Joe Kim <joesm.genie@gmail.com>
2025-03-26 23:00:45 +0700

    basic input/output

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int age;
+
+    cout << "Please enter your age: ";
+    cin >> age;
+
+    cout << "You are " << age << " years old.";
+
+    return 0;
+}
